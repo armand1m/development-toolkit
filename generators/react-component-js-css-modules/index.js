86efe44249f35f8570478e463085344f3ce4fcc3
Armando Magalhaes <amagalhaes@travix.com>
2019-10-29 17:42:50 +0100

    something

---
+++ generators/react-component-js-css-modules/index.js
@@ -0,0 +1,74 @@
+const path = require('path');
+
+const componentExists = require('../utils/component-exists');
+
+const componentTypes = require('./types');
+const mapTypeToTemplate = require('./map-type-to-template');
+
+const componentChoices = Object.values(componentTypes);
+
+const workingDir = process.cwd();
+
+module.exports = {
+  description: 'Add an unconnected React component',
+  prompts: [{
+    type: 'list',
+    name: 'type',
+    message: 'Select the type of component',
+    default: componentChoices[0],
+    choices: () => componentChoices,
+  }, {
+    type: 'input',
+    name: 'name',
+    message: 'What should it be called?',
+    default: 'Button',
+  }, {
+    type: 'input',
+    name: 'description',
+    message: 'How would you describe this component behavior?',
+    default: 'Renders a button component',
+  }, {
+    type: 'directory',
+    name: 'destiny',
+    message: 'Where you like to put this component?',
+    basePath: workingDir
+  }],
+  actions: (data) => {
+    const {
+      type,
+      name,
+      destiny,
+    } = data
+
+    if ((/.+/).test(name) && componentExists(destiny, name)) {
+      throw new Error('A component or container with this name already exists in this folder. Please try another destiny or another name.');
+    }
+
+    const target = path.join(workingDir, destiny);
+
+    const getTargetPathFor = (file) => `${target}/{{properCase name}}/${file}`;
+    const getTemplatePathFor = (file) => `./generators/react-component-js-css-modules/templates/${file}`;
+
+    return [{
+      type: 'add',
+      path: getTargetPathFor('index.js'),
+      templateFile: getTemplatePathFor(mapTypeToTemplate(type)),
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: getTargetPathFor('index.test.js'),
+      templateFile: getTemplatePathFor('test.js.hbs'),
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: getTargetPathFor('index.stories.js'),
+      templateFile: getTemplatePathFor('story.js.hbs'),
+      abortOnFail: true,
+    }, {
+      type: 'add',
+      path: getTargetPathFor('index.css'),
+      templateFile: getTemplatePathFor('index.css.hbs'),
+      abortOnFail: true,
+    }];
+  },
+};
---
+++ generators/react-component-ts/types.js
@@ -0,0 +1,13 @@
+const STATELESS = 'Stateless Function';
+const STATELESS_ARROW = 'Stateless Arrow Function';
+const STATEFUL = 'Stateful Class';
+const STATEFUL_PURE = 'Stateful Class (PureComponent)';
+const REDUX_CONNECTED_STATELESS = 'Redux Connected Stateless Function';
+
+module.exports = {
+  STATELESS,
+  STATELESS_ARROW,
+  STATEFUL,
+  STATEFUL_PURE,
+  REDUX_CONNECTED_STATELESS,
+};
